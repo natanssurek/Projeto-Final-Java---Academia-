ecca08756cd8dfac8c88ff74b608a3322d89a633
Natan <natanssurek@hotmail.com>
2026-06-09 20:26:27 -0300

    Criando a classe Pagamento (abstract)

--- src/Model/Pagamento.java
+++ src/Model/Pagamento.java
@@ -4,5 +4,50 @@ public abstract class Pagamento {
 
     private String id;
     /*private Aluno aluno;*/
+    private float valor;
+    private String data;
+    private boolean status;
 
+    public Pagamento(String id, float valor, String data, boolean status) {
+        this.id = id;
+        this.valor = valor;
+        this.data = data;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public abstract void calcularTotal();
+
+    public abstract void tipoPagamento();
 }
